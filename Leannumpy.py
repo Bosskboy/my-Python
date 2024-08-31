d562476269a2dae8d0b8d0bc63465a3100927c88
Bosskboy <kokazono380@gmail.com>
2024-08-31 23:05:53 +0700

    python2

---
+++ Leannumpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# arr = np.array(7)
+
+# print(arr)
+
+# 2_D array
+# arr = np.array([[2,4,6],[5,8,9]])
+# print(arr[1,2])
+
+# 3-D array
+arr = np.array([[[3,4,5],[6,7,8]],[[1,2,9],[4,6,9,]]])
+print(arr[1,1,2])

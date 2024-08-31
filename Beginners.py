e6fb88b16f8ef901d0f82566e35b3cabcf6dcc0d
Bosskboy <kokazono380@gmail.com>
2024-08-31 23:03:35 +0700

    My python

---
+++ Beginners.py
@@ -0,0 +1,87 @@
+#  integer
+# x = 10 
+# c = "Hello world "
+#  operater veraible
+# y = str(x) + c
+# print (y)
+#  size of veraible 
+# print(type(n))
+#  number complec
+# a = 7 + 4j
+# b = 5 + 7j
+# c= a + b
+# print(c)
+#  boolean
+# x = True
+# y = False
+#  modilo
+# x = 10 % 7
+#  xsponosel
+# x = 10**4
+#  incrementtol
+# x += 20
+# b = 500
+#   camparision operitor
+# y = x > b
+
+# print(y)
+# f(x)= x + 2
+# x ia paramater
+
+# f(2) = 2 + 2
+# 2 is agument
+
+
+# def f(x):
+#     return x + 2
+# result = f(7)
+# def charater(c):
+#     c = "this answer is " + str (c)
+#     print (c)
+# charater(result)
+
+# def f(x):
+#     return x * 5
+# result = f(7)
+# def charater (c):
+#     c= "This is answer : " + str(c)
+#     print(c) 
+# charater(result)
+# a = 3**2
+# b = 7**2
+# c = a + b
+# print (c)
+# if a == b:
+#     print("The numer ")
+# elif a != b:
+#     print("Leter a small than b")
+
+# list
+
+# a = ["Name :","Gender"]
+# b = ["chan That","K.boy","male","famale"]
+
+# for i in a:
+#     print(i)
+
+# cout = 0
+# while cout < len(a):
+#     print(" hello world")
+#     cout += 1
+# for i in range(len(b)):
+#     print(b[i])
+# mytuple = (3,4,5,6,6)
+# print(mytuple)
+# set = {5,3,5,9,2,"n "," kboy"}
+
+# for i in range(len(set)):
+#     print(i,set)
+# disenery = {1: "khmer",2:"english",3:"math"}
+# for result in range(len(disenery)):
+#     print(disenery.get(result))
+
+# couple of dictionary
+# dictionary = {1: "khmer",2:"english",3:"math"}
+# for item in dictionary.items():
+#     print(item)
+
